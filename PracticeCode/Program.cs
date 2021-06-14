236ccc30b0115895d4959192740b19748d7ef8f2
Bethanii <BethaniHampton@gmail.com>
2021-06-14 07:35:20 -0500

    inheritance practice

---
+++ PracticeCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class One
+    {
+        public int x = 5, y = 2;
+        public int addResult, subResult;
+        public int Add()
+        {
+            addResult = x + y;
+            return addResult;
+        }
+        public int Subtract()
+        {
+            subResult = x - y;
+            return subResult;
+        }
+    }
+
+    class Two : One
+    {
+        public int multResult;
+        public int Multiply()
+        {
+            multResult = Add() * Subtract();
+            return multResult;
+        }
+    }
+    class Three
+    {   
+    static void Main(string[] args)
+       {
+            One myOne = new One();
+            Two myTwo = new Two();
+
+            Console.WriteLine("5 + 2 = " + myOne.Add() + "\n" + "5 - 2 = " + myTwo.Subtract() 
+                + "\n" + "5 * 2 = " + myTwo.Multiply());
+        }
+    }
+}
